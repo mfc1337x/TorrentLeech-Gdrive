a17441034f951c7ce484114a8f987790951058b1
mfc1337x <59104299+mfc1337x@users.noreply.github.com>
2020-07-22 02:26:53 +0200

    Update new_join_fn.py

--- tobrot/plugins/new_join_fn.py
+++ tobrot/plugins/new_join_fn.py
@@ -39,25 +39,7 @@ async def help_message_f(client, message):
     # display the /help
     
     await message.reply_text("""Kya he bhosdike? Call @iclapcheeks for help otherwise jake apni gand marale!
-    First sand link then reply with one of the below commands.
-    
-    /ytdl: This command should be used as reply to a supported link
-    /ytdl gdrive: This will download and upload to your cloud.
-    /leech: This command should be used as reply to a magnetic link, a torrent link, or a direct link. [this command will SPAM the chat and send the downloads a seperate files, if there is more than one file, in the specified torrent]
-    /leech archive: This command should be used as reply to a magnetic link, a torrent link, or a direct link. [This command will create a .tar.gz file of the output directory, and send the files in the chat, splited into PARTS of 1024MiB each, due to Telegram limitations]
-    /gleech: This command should be used as reply to a magnetic link, a torrent link, or a direct link. And this will download the files from the given link or torrent and will upload to the cloud using rclone.
-    /gleech archive This command will compress the folder/file and will upload to your cloud.
-    /leech unzip: This will unzip the .zip file and dupload to telegram.
-    /gleech unzip: This will unzip the .zip file and upload to cloud.
-    /leech unrar: This will unrar the .rar file and dupload to telegram.
-    /gleech unrar: This will unrar the .rar file and upload to cloud.
-    /leech untar: This will untar the .tar file and upload to telegram.
-    /gleech untar: This will untar the .tar file and upload to cloud..
-    /tleech: This will mirror the telegram files to ur respective cloud cloud.
-    /tleech unzip: This will unzip the .zip telegram file and upload to cloud.
-    /tleech unrar: This will unrar the .rar telegram file and upload to cloud.
-    /tleech untar: This will untar the .tar telegram file and upload to cloud.
-    /getsize: This will give you total size of your destination folder in cloud.""")
+    First sand link then reply with a command""")
 
 
 async def rename_message_f(client, message):
